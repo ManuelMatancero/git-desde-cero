2b731ca6f7ea57ff9becd46128eb23e7affcdfb2
Manuel Antonio Sarante <20210785@itla.edu.do>
2021-11-22 22:08:17 -0400

    Modificando el archivo java por segunda vez

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,6 +2,8 @@ HolaMundo.java
 
 Package matancita;
 
+public class Matanica{
+
 public static void public static void main(String[] args) {
 	
  		System.out.println("HolaMundo"); 
@@ -9,4 +11,10 @@ public static void public static void main(String[] args) {
 //Git es un sistema de control de versiones que nos ayuda a llevar el historial completo
  		//de modificaciones de un proyecto, conociento los detalle de quien y cuando se hizo
 
+//Hola Mi nombre es bond huebon
+ 		
+
+    }
+
 }
+
