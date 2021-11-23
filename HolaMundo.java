62636ec0924fd3f37e65eb5fbb303c6b547ae561
Manuel Antonio Sarante <20210785@itla.edu.do>
2021-11-22 22:00:16 -0400

    Modificando el archivo .java

--- HolaMundo.java
+++ HolaMundo.java
@@ -6,5 +6,7 @@ public static void public static void main(String[] args) {
 	
  		System.out.println("HolaMundo"); 
 
+//Git es un sistema de control de versiones que nos ayuda a llevar el historial completo
+ 		//de modificaciones de un proyecto, conociento los detalle de quien y cuando se hizo
 
 }
